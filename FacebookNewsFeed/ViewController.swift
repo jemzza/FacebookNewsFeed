52d407b655feae561fa8213b0cbb7be3c043882c
Alexander Litvinov <allidemocracy@gmail.com>
2020-03-18 18:37:16 +0300

    Initial Commit

---
+++ FacebookNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookNewsFeed
+//
+//  Created by Alexander on 18.03.2020.
+//  Copyright © 2020 Alexander Litvinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

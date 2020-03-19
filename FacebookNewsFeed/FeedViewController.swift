93dc687ed5350163a2a167e5ab74acd77d430ef1
Alexander Litvinov <allidemocracy@gmail.com>
2020-03-19 23:36:12 +0300

    add status text

---
+++ FacebookNewsFeed/FeedViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  FacebookNewsFeed
+//
+//  Created by Alexander on 18.03.2020.
+//  Copyright © 2020 Alexander Litvinov. All rights reserved.
+//
+
+import UIKit
+
+let cellId = "cellId"
+
+class FeedViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "FaceBook Feed"
+        collectionView.alwaysBounceVertical = true
+        UIApplication.shared.statusBarStyle = .lightContent
+        collectionView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        
+        collectionView.register(FeedCell.self, forCellWithReuseIdentifier: cellId)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+}
+
+class FeedCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        let attributedText = NSMutableAttributedString(string: "Mark Zuckerberg", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "\nDecember 18  •  San Francisco  •  ", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12), NSAttributedString.Key.foregroundColor: UIColor(red: 155 / 255, green: 161 / 255, blue: 171 / 255, alpha: 1)]))
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 4
+        
+        attributedText.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedText.string.count))
+        
+        // include imageViews inside your text
+        let attacment = NSTextAttachment()
+        attacment.image = UIImage(named: "globe_small")
+        attacment.bounds = CGRect(x: 0, y: -2, width: 12, height: 12)
+        attributedText.append(NSAttributedString(attachment: attacment))
+        
+        label.attributedText = attributedText
+        return label
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "zuckprofile")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let statusTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Meanwhile, Beast turned to the dark side."
+        textView.font = UIFont.systemFont(ofSize: 14)
+        return textView
+    }()
+    
+    func setupViews() {
+        backgroundColor = .white
+        
+        addSubview(nameLabel)
+        addSubview(profileImageView)
+        addSubview(statusTextView)
+        
+        addConstraintsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]|", views: profileImageView ,nameLabel)
+//        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0(44)]-8-[v1]|", metrics: nil, views: ["v0": profileImageView, "v1": nameLabel]))
+        addConstraintsWithFormat(format: "H:|-4-[v0]-4-|", views: statusTextView)
+        addConstraintsWithFormat(format: "V:|-12-[v0]", views: nameLabel)
+//        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", metrics: nil, views: ["v0": nameLabel]))
+        addConstraintsWithFormat(format: "V:|-8-[v0(44)]-4-[v1(30)]", views: profileImageView, statusTextView)
+//        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0(44)]", metrics: nil, views: ["v0": profileImageView]))
+
+    }
+}
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: format, metrics: nil, views: viewsDictionary))
+    }
+}
